69e236db09a6a0f08f4d2631b84d9bded096cc41
Conor Perreault <cperreault@wisc.edu>
2022-01-19 11:13:28 +0000

    working copy

--- snorkel/learning/pytorch/__init__.py
+++ snorkel/learning/pytorch/__init__.py
@@ -3,3 +3,4 @@ from __future__ import absolute_import
 from .logistic_regression import LogisticRegression
 from .noise_aware_model import TorchNoiseAwareModel
 from .rnn import LSTM
+from .rnn import LSTM_orig
--- snorkel/learning/pytorch/rnn/__init__.py
+++ snorkel/learning/pytorch/rnn/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import absolute_import
 
 from .lstm import LSTM
+
+from .lstm_orig import LSTM_orig
